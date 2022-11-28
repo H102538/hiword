410699591f6c81418a2f535b38a704f2c44b2cf7
itcast <you@example.com>
2022-11-28 21:53:18 +0800

    Merge branch 'master' of https://github.com/H102538/hiword
    # Conflicts:
    #	.idea/compiler.xml
    #	pom.xml

--- src/main/java/tset.java
+++ src/main/java/tset.java
@@ -2,5 +2,10 @@ public class tset {
     public static void main(String[] args) {
 
         System.out.println("11111111111");
+
+        System.out.println("544564685745");
+        System.out.println("544564685745");
+        System.out.println("544564685745");System.out.println("544564685745");
+
     }
 }
